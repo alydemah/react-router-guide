b7469b6307bbe6371e43300d88c0e7891528f026
alydemah <alydemah@gmail.com>
2017-12-28 13:54:43 +0100

    Installing React Router and Declaring Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Home from './Home';
+import About from './About';
+import Header from './Header';
+import Teachers from './Teachers';
+import Courses from './Courses';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
+
+// Beign by rendering a route that wrap all the app components
+
 
 const App = () => (
+  <BrowserRouter>
   <div className="container">
+    <Header />
+  <Route exact path="/" component={Home} />
+  <Route path="/about" component={About} />
+  <Route path="/teachers" component={Teachers} />
+  <Route path="/courses" component={Courses} />
 
   </div>
+  </BrowserRouter>
 );
 
 export default App;
